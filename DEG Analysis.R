ffea0c1b98f32ccb73781d488e6f34b35090b19d
Hunter Eby <hunter.eby@rockets.utoledo.edu>
2024-05-06 15:42:01 -0400

    Me and john struggle bus

--- DEG Analysis.R
+++ DEG Analysis.R
@@ -9,3 +9,20 @@ tbl <- as.matrix(data.table::fread(path, header=T, colClasses="integer"), rownam
 
 #Looks into package TiSA, and other time series packages to do in this analysis
 #DEG using time series. 
+BiocManager::install("GEOmeta")
+X <- getGEO("GSE86884")
+library(tidyverse)
+
+# Assuming 'tbl' is your existing data frame
+# Create a vector of column names
+column_names <- colnames(tbl)
+
+# Create a data frame with the desired structure
+new_df1 <- tibble(
+  sample = rep(column_names),  # Repeat each column name 4 times
+  group = rep("transplant", times = length(column_names)),  # Fill 'group' column with "transplant"
+  # Setting up the replicate column
+  total_col <- do.call(rbind, lapply(FUN=function(x) rep('transplant', 1:4), X=1:24))
+  
+# View the new data frame
+print(new_df)
