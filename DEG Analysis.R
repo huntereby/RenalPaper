901b199b43e3f7140ee18e32a7115bd5bf20c4c6
rela <rela.mngrbot@gmail.com>
2024-05-06 16:10:06 -0400

    fixed tibble confirmed

--- DEG Analysis.R
+++ DEG Analysis.R
@@ -1,6 +1,16 @@
 #R script for grabbing the dataset
-library(GEOquery)
 
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+bio_pkgs <- c('DESeq2','GOSemSim','GO.db','limma','ComplexHeatmap',
+'AnnotationDbi','GenomeInfoDb','GenomicRanges','Biobase','S4Vectors',
+'BiocGenerics','MatrixGenerics','IRanges','BiocFileCache',
+'SummarizedExperiment','org.Hs.eg.db','org.Mm.eg.db','org.Ce.eg.db',
+'tximport','tximportData', 'GEOquery')
+BiocManager::install(bio_pkgs)
+install.packages("devtools")
+devtools::install_github("Ylefol/TimeSeriesAnalysis@master")
+library(GEOquery)
 
 # load counts table from GEO
 urld <- "https://www.ncbi.nlm.nih.gov/geo/download/?format=file&type=rnaseq_counts"
@@ -21,13 +31,9 @@ column_names <- colnames(tbl)
 new_df1 <- tibble(
   sample = rep(column_names),  # Repeat each column name 4 times
   group = rep("transplant", times = length(column_names)),  # Fill 'group' column with "transplant"
-  replicate = rep(paste0("transplant_", 1:96), each = 4),
+  # Replicate "transplant_1" 4 times, then "transplant_2" 4 times, and so on until there's 96 rows total
+  replicate = rep(paste0("transplant_", rep(1:24, each = 4)), times = 1),
   time_point = rep(c('preop', '3wk', '3mo', '7mo'), 24))
-  # Setting up the replicate column
-  # Repeat the string "transplant_1" 4 times, then "transplant_2" 4 times, and so on 
-  
-  # Combine columns of total_col into a single column
-  
 
-  
-# View the new data frame
+# Place this command in the terminal:
+# Rscript -e "rmarkdown::render('rmarkdown_method/TS_analysis.Rmd',output_file='TS_analysis.html')"
