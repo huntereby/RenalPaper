86563a4c87e80ef3374e82dc9e6f4dba13edae51
Hunter Eby <hunter.eby@rockets.utoledo.edu>
2024-05-06 12:18:25 -0400

    Little to no update

--- DEG Analysis.R
+++ DEG Analysis.R
@@ -1,1 +1,11 @@
 #R script for grabbing the dataset
+library(GEOquery)
+
+
+# load counts table from GEO
+urld <- "https://www.ncbi.nlm.nih.gov/geo/download/?format=file&type=rnaseq_counts"
+path <- paste(urld, "acc=GSE86884", "file=GSE86884_raw_counts_GRCh38.p13_NCBI.tsv.gz", sep="&");
+tbl <- as.matrix(data.table::fread(path, header=T, colClasses="integer"), rownames=1)
+
+#Looks into package TiSA, and other time series packages to do in this analysis
+#DEG using time series. 
