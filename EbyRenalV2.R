0753af1e38e770d4e447bfa2af2e4dfd8711da20
Hunter Eby <hunter.eby@rockets.utoledo.edu>
2024-06-27 15:32:53 -0400

    Time sereis analysis to be added to 3pod 4 analysis

--- EbyRenalV2.R
+++ EbyRenalV2.R
@@ -38,93 +38,18 @@ ds <- DESeqDataSetFromMatrix(countData=tbl, colData=sample_info, design= ~Group)
 #1 Week comparison
 res_1_week <- results(ds, contrast=c("Group", groups[1], groups[2]), alpha=0.05, pAdjustMethod = "fdr")
 res_1_week <- merge(as.data.frame(res_1_week), annot, by=0, sort=F)
-res_1_week <- subset(res_1_week, select=c("GeneID","padj","pvalue","stat","baseMean","Symbol","Description"))
+res_1_week <- subset(res_1_week, select=c("Symbol","log2FoldChange","pvalue"))
+res_1_week
+write.csv(res_1_week, "1weekComp.csv")
 #3 month comparison
 res_3_week <- results(ds, contrast=c("Group", groups[1], groups[3]), alpha=0.05, pAdjustMethod = "fdr")
 res_3_week <- merge(as.data.frame(res_3_week), annot, by=0, sort=F)
-res_3_week <- subset(res_3_week, select=c("GeneID","padj","pvalue","stat","baseMean","Symbol","Description"))
+res_3_week <- subset(res_3_week, select=c("Symbol","log2FoldChange","pvalue"))
+res_3_week
+write.csv(res_3_week, "3MonthComp.csv")
 #6 month comparison
 res_6_week <- results(ds, contrast=c("Group", groups[1], groups[4]), alpha=0.05, pAdjustMethod = "fdr")
 res_6_week <- merge(as.data.frame(res_6_week), annot, by=0, sort=F)
-res_6_week <- subset(res_6_week, select=c("GeneID","padj","pvalue","stat","baseMean","Symbol","Description"))
-
-
-
-
-
-ds <- DESeq(ds, test="LRT", reduced = ~ 1)  # Use LRT for all-around gene ranking
-
-# extract results for top genes table
-r <- results (ds, alpha=0.05, pAdjustMethod ="fdr")
-
-tT <- r[order(r$padj)[],] 
-tT <- merge(as.data.frame(tT), annot, by=0, sort=F)
-
-tT <- subset(tT, select=c("GeneID","padj","pvalue","stat","baseMean","Symbol","Description"))
-write.table(tT, file=stdout(), row.names=F, sep="\t")
-
-plotDispEsts(ds, main="GSE86884 Dispersion Estimates")
-
-# create histogram plot of p-values
-hist(r$padj, breaks=seq(0, 1, length = 21), col = "grey", border = "white", 
-     xlab = "", ylab = "", main = "GSE86884 Frequencies of padj-values")
-
-# Wald test to obtain contrast-specific results
-ds <- DESeq(ds, test="Wald", sfType="poscount")
-r <- results (ds, contrast=c("Group", groups[1], groups[4]), alpha=0.05, pAdjustMethod = "fdr")
-
-# volcano plot
-old.pal <- palette(c("#00BFFF", "#FF3030")) # low-hi colors
-par(mar=c(4,4,2,1), cex.main=1.5)
-plot(r$log2FoldChange, -log10(r$padj), main=paste(groups[1], "vs", groups[4]),
-     xlab="log2FC", ylab="-log10(Padj)", pch=20, cex=0.5)
-with(subset(r, padj<0.05 & abs(log2FoldChange) >= 0),
-     points(log2FoldChange, -log10(padj), pch=20, col=(sign(log2FoldChange) + 3)/2, cex=1))
-legend("bottomleft", title=paste("Padj<", 0.05, sep=""), legend=c("down", "up"), pch=20,col=1:2)
-
-# MD plot
-par(mar=c(4,4,2,1), cex.main=1.5)
-plot(log10(r$baseMean), r$log2FoldChange, main=paste(groups[1], "vs", groups[2]),
-     xlab="log10(mean of normalized counts)", ylab="log2FoldChange", pch=20, cex=0.5)
-with(subset(r, padj<0.05 & abs(log2FoldChange) >= 0),
-     points(log10(baseMean), log2FoldChange, pch=20, col=(sign(log2FoldChange) + 3)/2, cex=1))
-legend("bottomleft", title=paste("Padj<", 0.05, sep=""), legend=c("down", "up"), pch=20,col=1:2)
-abline(h=0)
-palette(old.pal) # restore palette
-
-# Venn diagram
-library(gplots)
-all_res <- list()
-
-ct.names <- resultsNames(ds)[-1] # contrasts names without Intercept
-for (ct in ct.names) {
-  r <- results(ds, name=ct, alpha=0.05, pAdjustMethod = "fdr")
-  all_res[[length(all_res) + 1]] <- rownames(r)[!is.na(r$padj) & r$padj < 0.05 & abs(r$log2FoldChange) >= 0]
-}
-names(all_res) <- ct.names
-
-venn(all_res)
-
-################################################################
-#   General expression data visualization
-dat <- log10(counts(ds, normalized = T) + 1) # extract normalized counts
-
-# box-and-whisker plot
-lbl <- "log10(raw counts + 1)"
-dev.new(width=3+ncol(gset)/6, height=5)
-ord <- order(gs)  # order samples by group
-palette(c("#1B9E77", "#7570B3", "#E7298A", "#E6AB02", "#D95F02",
-          "#66A61E", "#A6761D", "#B32424", "#B324B3", "#666666"))
-par(mar=c(7,4,2,1))
-boxplot(dat[,ord], boxwex=0.6, notch=T, main="GSE86884", ylab="lg(norm.counts)", outline=F, las=2, col=gs[ord])
-legend("topleft", groups, fill=palette(), bty="n")
-dev.off()
-
-# UMAP plot (multi-dimensional scaling)
-library(umap)
-dat <- dat[!duplicated(dat), ] # first remove duplicates
-par(mar=c(3,3,2,6), xpd=TRUE, cex.main=1.5)
-ump <- umap(t(dat), n_neighbors = 15, random_state = 123)
-plot(ump$layout, main="UMAP plot, nbrs=15", xlab="", ylab="", col=gs, pch=20, cex=1.5)
-legend("topright", inset=c(-0.15,0), legend=groups, pch=20,
-       col=1:length(groups), title="Group", pt.cex=1.5)
+res_6_week <- subset(res_3_week, select=c("Symbol","log2FoldChange","pvalue"))
+res_6_week
+write.csv(res_6_week, "6MonthComp.csv")
